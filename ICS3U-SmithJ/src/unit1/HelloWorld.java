9e0d41f60f02615a63e98dcfb81b64908f569da4
350256423 <350256423@C-MXL23835B1.tdsb.on.ca>
2024-09-20 11:11:06 -0400

    Adding ICS3U project with HelloWorld class

---
+++ ICS3U-SmithJ/src/unit1/HelloWorld.java
@@ -0,0 +1,20 @@
+package unit1;
+/**
+ * Description: This program prints Hello World
+ * Date: Sept. 19, 2024
+ * @author Jude Smith
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		System.out.println("Hello World!!");
+        System.out.println("My name is King Jonas");
+
+        /*
+         * This is a block comment.
+         * You can add notes on multiple lines.
+         */
+	}
+
+}
