d8e977a3c97ea104c5b9d8901a7dd3884a2e52d0
350256423 <350256423@C-MXL23835B1.tdsb.on.ca>
2024-09-20 11:38:16 -0400

    I told some things about me

---
+++ ICS3U-SmithJ/src/unit1/AboutMe.java
@@ -0,0 +1,17 @@
+package unit1;
+/**
+ * Description: This prints information about me
+ * Date: 20/09/24
+ * @author Jude Smith
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// This prints about me to the system console
+		System.out.println("Jude Smith");
+		System.out.println("I am in Grade 11");
+		System.out.println("My teacher is Mrs. Kemp");
+		System.out.println("I go to Lawrence Park");
+	}
+
+}
