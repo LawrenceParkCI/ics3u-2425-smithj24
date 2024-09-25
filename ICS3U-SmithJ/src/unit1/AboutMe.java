182facc6180e6f6fc1ae8e362b8c82d632c3ed33
350256423 <350256423@C-MXL23835B1.tdsb.on.ca>
2024-09-25 10:54:06 -0400

    Finished the whole timetable

--- ICS3U-SmithJ/src/unit1/AboutMe.java
+++ ICS3U-SmithJ/src/unit1/AboutMe.java
@@ -12,6 +12,12 @@ public class AboutMe {
 		System.out.println("I am in Grade 11");
 		System.out.println("My teacher is Mrs. Kemp");
 		System.out.println("I go to Lawrence Park");
-	}
+        System.out.println("\"Let's go Panthers\"");
+        System.out.format("Timetable for week of September 23");
+	    System.out.format("\"Date: %15sTime: %8s\"" , "Monday" , "9:00-10:15");
+	    System.out.format("Date: %15sTime: %8s" , "Monday" , "10;20-11:40");
+        System.out.format("Date: %15sTime: %8s" , "Monday" , "12:40-1:55");
+}
 
+	
 }
