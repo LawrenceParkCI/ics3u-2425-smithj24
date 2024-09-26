27535367b077ff21b418de09071d72e0bdf542c9
350256423 <350256423@C-MXL23835B1.tdsb.on.ca>
2024-09-26 11:36:22 -0400

    Wrote down the rules

--- ICS3U-SmithJ/src/unit1/AboutMe.java
+++ ICS3U-SmithJ/src/unit1/AboutMe.java
@@ -14,10 +14,40 @@ public class AboutMe {
 		System.out.println("I go to Lawrence Park");
         System.out.println("\"Let's go Panthers\"");
         System.out.format("Timetable for week of September 23");
-	    System.out.format("\"Date: %15sTime: %8s\"" , "Monday" , "9:00-10:15");
-	    System.out.format("Date: %15sTime: %8s" , "Monday" , "10;20-11:40");
-        System.out.format("Date: %15sTime: %8s" , "Monday" , "12:40-1:55");
-}
+	    System.out.println("______________________________________");
+	    System.out.println("Monday");
+	    System.out.format("Class: %-15s | Time: %8s", "Computer Science", "9:00-10:15 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "IAPS", "10:20-11:40 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "American History", "12:40-1:55 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "English", "2:00-3:15 \n");
+	    
+	    System.out.println("Tuesday");
+	    System.out.format("Class: %-15s | Time: %8s", "IAPS", "9:00-10:15 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "Computer Science", "10:20-11:40 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "Engish", "12:40-1:55 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "American History", "2:00-3:15 \n");
+	    
+	    System.out.println("Wednesday");
+	    System.out.format("Class: %-15s | Time: %8s", "Computer Science", "9:55-10:55 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "IAPS", "11:00-12:00 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "American History", "1:00-2:05 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "English", "2:10-3:15 \n");
+	    
+	    System.out.println("Thursday");
+	    System.out.format("Class: %-15s | Time: %8s", "IAPS", "9:00-10:15 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "Computer Science", "10:20-11:40 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "English", "12:40-1:55 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "American History", "2:00-3:15 \n");
+	    
+	    System.out.println("Friday");
+	    System.out.format("Class: %-15s | Time: %8s", "Computer Science", "9:00-10:15 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "IAPS", "10:20-11:40 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "American History", "12:40-1:55 \n");
+	    System.out.format("Class: %-16s | Time: %8s", "English", "2:00-3:15 \n");
+	    
+	    
+	    
+	}
 
 	
 }
--- ICS3U-SmithJ/src/unit1/SchoolTimetble.java
+++ ICS3U-SmithJ/src/unit1/SchoolTimetble.java
@@ -11,7 +11,7 @@ public class SchoolTimetble  {
 		System.out.println("3. Players look for the called number on their bingo card. \n");
 		System.out.println("4. If the number is located, it is marked off. \n");
 		System.out.println("5. Steps 1 to 4 are repeated until a player matches the BINGO pattern. \n");
-	    System.out.println("6. The winning player yells BINGO");
+	    System.out.println("6. The winning player yells BINGO"); 
 	
 	}
 
