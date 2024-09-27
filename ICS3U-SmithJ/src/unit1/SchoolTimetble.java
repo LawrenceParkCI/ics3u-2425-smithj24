0b23f542909df60e4f29a87ee43c03a677d66433
350256423 <350256423@C-MXL23835B1.tdsb.on.ca>
2024-09-27 10:08:59 -0400

    I have finished the rules on how to play BINGO

--- ICS3U-SmithJ/src/unit1/SchoolTimetble.java
+++ ICS3U-SmithJ/src/unit1/SchoolTimetble.java
@@ -12,6 +12,26 @@ public class SchoolTimetble  {
 		System.out.println("4. If the number is located, it is marked off. \n");
 		System.out.println("5. Steps 1 to 4 are repeated until a player matches the BINGO pattern. \n");
 	    System.out.println("6. The winning player yells BINGO"); 
+	    System.out.println("_________________________________________________________________________________");
+   //rules of bingo
+	
+	    
+	    System.out.print("B");
+	    System.out.print("\t");
+	    System.out.print("I");
+	    System.out.print("\t");
+	    System.out.print("N");
+	    System.out.print("\t");
+        System.out.print("G");
+        System.out.print("\t");
+	    System.out.print("O");
+	
+	    
+	    System.out.format("\n2 %8s %8s %8s %8s" , "20" , "42" , "60" , "64");
+        System.out.format("\n14 %8s %8s %8s %8s" , "14" , "25" , "32" , "55" , "70");
+	    System.out.format("\n5 %8s %8s %8s %8s" , "18" , "FREE" , "53" , "67");
+	    System.out.format("\n12 %8s %8s %8s %8s", "12" , "16" , "31" , "46" , "75");
+	    System.out.format("\n10 %8s %8s %8s %8s", "10" , "22" , "39" , "59" , "71");
 	
 	}
 
