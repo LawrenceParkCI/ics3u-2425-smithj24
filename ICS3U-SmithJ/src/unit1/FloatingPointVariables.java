f652497bd08964ef099971fe2e9de362c81770cb
350256423 <350256423@C-MXL23835B1.tdsb.on.ca>
2024-10-03 10:12:00 -0400

    Completed floating variables

---
+++ ICS3U-SmithJ/src/unit1/FloatingPointVariables.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package unit1;
+
+/**
+ * 
+ */
+public class FloatingPointVariables {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
