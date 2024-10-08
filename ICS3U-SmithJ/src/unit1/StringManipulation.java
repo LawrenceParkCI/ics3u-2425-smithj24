1264dbe97d9158b6a6d93e799104f02bedf7099f
350256423 <350256423@C-MXL23835B1.tdsb.on.ca>
2024-10-08 11:24:45 -0400

    I have been alive for 16.14246575342466 years I have been alive for 5,892 days I have slept for 47136 hours in my lifetime

---
+++ ICS3U-SmithJ/src/unit1/StringManipulation.java
@@ -0,0 +1,39 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class StringManipulation {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s;
+		
+		System.out.println("Type in a sentence, and press enter>");
+		s = sc.nextLine();
+		System.out.println("This is the string normally");
+		System.out.println(s);
+		System.out.println("This is the string in uppercase letters");
+		System.out.println((s.toUpperCase()));
+		System.out.println("This is the string in lowercase letters");
+		System.out.println((s.toLowerCase()));
+		System.out.println("This is the length of the string");
+		System.out.println((s.length()));
+		System.out.println("This is the character at index 5");
+		System.out.println(s.charAt(5));
+		sc.close();
+		
+		
+		//getting the first and last space
+		int firstSpace = s.indexOf(" ");
+		int lastSpace = s.lastIndexOf(" ");
+		//first and last word
+		System.out.println("")
+		
+		
+		
+		
+	}
+
+}
